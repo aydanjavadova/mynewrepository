60bf1e342773542161a97e44d8ce59535e1f13c5
Aidan <aydancavadova94@gmail.com>
2025-02-15 20:49:54 +0400

    first project attached

---
+++ src/main/java/az/edu/turing/module01/lesson03/MovieApp.java
@@ -0,0 +1,7 @@
+package az.edu.turing.module01.lesson03;
+
+public class MovieApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/az/edu/turing/module01/lesson04/TestApp.java
@@ -0,0 +1,7 @@
+package az.edu.turing.module01.lesson04;
+
+public class TestApp {
+    public static void main(String[] args) {
+
+    }
+}
