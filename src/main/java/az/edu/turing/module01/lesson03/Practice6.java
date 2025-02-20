f9ec10c7935c0f10df02d9873572f25d45ba64e8
Aidan <aydancavadova94@gmail.com>
2025-02-20 17:56:37 +0400

    ArrayDemo3.java attached

---
+++ src/main/java/az/edu/turing/module01/lesson03/Practice6.java
@@ -0,0 +1,12 @@
+package az.edu.turing.module01.lesson03;
+
+public class Practice6 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 3; i++) {
+            sum += i * 2;
+        }
+        System.out.println(sum);
+    }
+}
+
